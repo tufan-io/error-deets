64d281f813e2ce686297b2cdefb8b3a9ecb3594b
sramam <sramam@gmail.com>
2020-05-18 11:13:20 -0700

    fix: convert \ -> / in stack traces. Helps cross-os snapshots

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,16 @@ class ErrorDeets extends Error {
         }, opts);
         this.details = details;
         this.code = code;
+        // Maintainer warning:
+        // only one leg of this conditional can be exercised on an OS.
+        // So we ignore coverage after having tested this manually.
+        /* istanbul ignore next */
+        this.stack = !!this.stack
+            ? this.stack
+                .split("\n")
+                .map((l) => l.replace(/\\+/g, "/"))
+                .join("\n")
+            : this.stack;
         // Set the prototype explicitly.
         Object.setPrototypeOf(this, ErrorDeets.prototype);
     }
--- docs/cloc.md
+++ docs/cloc.md
@@ -3,6 +3,6 @@ cloc|github.com/AlDanial/cloc v 1.85
 
 File|blank|comment|code
 :-------|-------:|-------:|-------:
-./src/index.ts|3|1|19
+./src/index.ts|3|5|25
 --------|--------|--------|--------
-SUM:|3|1|19
+SUM:|3|5|25
